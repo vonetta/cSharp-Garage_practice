a57f2e60c2c6ce7852527b02770478cca3570f60
Vonetta <vonettastevenson@gmail.com>
2017-11-17 18:36:54 -0800

    Add project files.

---
+++ Garage/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public bool Sold { get; set; }
+    }
+}
---
+++ Garage/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a car class
+            //Make, Model, Color, Price, Sold
+
+            //create a list of cars(3-4 cars)
+            //build a for loop and output all the cars
+
+            List<Car> carList = new List<Car>();
+
+            //three ways to add a car
+            //1 - standard for user input
+            Car c1 = new Car()
+            {
+                Make = "BMW",
+                Model = "X5",
+                Color = "Blue",
+                Price = 1.00m,
+                Sold = false
+            };
+            carList.Add(c1);
+
+            //2
+            carList.Add(new Car { Make = "Audi", Model = "X4", Color = "Green", Price = 1.2m,
+                Sold = true });
+
+            //3
+            Car c3 = new Car();
+            c3.Make = "Toyota";
+            c3.Model = "Corolla";
+            c3.Color = "Gray";
+            c3.Price = 5m;
+            c3.Sold = true;
+
+            carList.Insert(0, c3); //adds c3 as the first car
+
+            //build a for loop
+
+            for (int i = 0; i < carList.Count; i++) {
+                /*
+                string sold = "";
+                if (carList[i].Sold)
+                {
+                    sold = "Sold";
+                }
+                else {
+                    sold = "Not sold";
+                }*/
+              //  Console.WriteLine("{0}, {1} is {2}, listed for {3:c}", carList[i].Make, carList[i].Model, carList[i].Sold ? "sold": "not sold", carList[i].Price);
+
+               // Console.WriteLine("{0}, {1} is {2}, listed for {3:c}", carList[i].Make, carList[i].Model, sold, carList[i].Price);
+            }
+
+            foreach (Car item in carList) {
+                Console.WriteLine("{0}, {1} is {2}, listed for {3:c}", carList[i].Make, carList[i].Model, carList[i].Sold ? "sold" : "not sold", carList[i].Price);
+            }
+            Console.ReadLine();
+        }
+    }
+}
